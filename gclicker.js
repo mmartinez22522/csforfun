07d2e0b0ab4c02947abddb5a0f7b229179da002e
mmartinez22522 <83504947+mmartinez22522@users.noreply.github.com>
2021-05-10 07:15:26 -0700

    Add files via upload

--- gclicker.js
+++ gclicker.js
@@ -1,15 +1,45 @@
 var score = 0;
 var numsenor = 0;
+var numfarm = 0;
+var infla = 0;
+var totalbot = 0;
+//var infl = 9;
 function buy(c)
 {
 if(c == 'senor')
+{
+    if(score > 9 + infla)
+    {
+        score = score - 10;
+        document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
+        numsenor++;
+        totalbot++;
+        document.getElementById("senor").innerHTML = "Un Senor: "+numsenor+" Senors";
+        if(numsenor > 2)
+        {
+            infla = totalbot*2;
+            var i = infla + 9;
+            document.getElementById("csenor").innerHTML = "Cost: " + i + " galletas"; 
+        }
+        autosenor();
+    }
+}
+if(c == 'farm')
 {
     if(score > 49)
     {
         score = score - 50;
         document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
-        numsenor++;
-        autosenor();
+        numfarm++;
+        totalbot++;
+        document.getElementById("farm").innerHTML = "Un Farm: "+numfarm+" Farms";
+        if(numfarm > 2)
+        {
+            infla = totalbot*2;
+            var i = infla + 49;
+            document.getElementById("cfarm").innerHTML = "Cost: " + i + " galletas";
+        }
+        autofarm();
     }
 }
 }
@@ -19,9 +49,15 @@ function umclick()
     //alert(score);
     document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
 }
-function autosenor();
+function autosenor()
 {
-    var point = numsenor*10;
-    score = score + point;
-    setTimeout(autosenor(),10000);
+    score = score + numsenor;
+    document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
+    setTimeout(autosenor,1000);
+}
+function autofarm()
+{
+    score = score + numfarm*2;
+    document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
+    setTimeout(autofarm,5000);
 }
